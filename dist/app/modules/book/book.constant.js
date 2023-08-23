296c038f2a437a8272cb07c452d695299a458bf5
Hasan-Rifat <developerhr000@gmail.com>
2023-08-23 18:30:51 +0600

    [build website for deployment]

---
+++ dist/app/modules/book/book.constant.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BookSearchableFields = void 0;
+exports.BookSearchableFields = [
+    'title',
+    'author',
+    'genre',
+    'publicationDate',
+    'reviews',
+    'email',
+    'searchTerm',
+];
